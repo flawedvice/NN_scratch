2ea9148509d206517c517b2fd75701efc548af26
Vicente Aranda <vicente.aranda@protonmail.com>
2023-06-15 14:12:10 -0400

    First commit

---
+++ formulas.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import typing
+
+def mse(theta, X, y_hat):
+    pass
+
+def gradient_vector(theta, X, y_hat):
+    pass
+
+def descent_step(theta_i, etha, gradient_vector):
+    pass
